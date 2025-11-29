fac98a21b98689fd025624bc62863fec693b9158
axrorback <axrorback@gmail.com>
2025-11-29 23:05:51 +0500

    Commit on Ubuntu

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import CustomJWTAPI
 from rest_framework_simplejwt.views import TokenRefreshView
 
+
 urlpatterns = [
     path('login/', CustomJWTAPI.as_view(),),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
